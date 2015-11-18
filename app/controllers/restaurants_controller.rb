38dd420ec1fc2995a2ec50e634dffec94fbfccf2
Carter-A <carter.s.andrew@gmail.com>
2015-11-18 14:33:48 -0500

    restaurant controller and new view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,22 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_url
+    else
+      render :new
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :location, :cuisine)
+  end
 end

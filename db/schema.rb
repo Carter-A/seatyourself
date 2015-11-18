fe71be81f5d1163a8b1b26fe9a01ce3e76665239
ZainW <zainthegreatcb@gmail.com>
2015-11-18 15:24:36 -0500

    add login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117195921) do
+ActiveRecord::Schema.define(version: 20151118201612) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "party_size"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20151117195921) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

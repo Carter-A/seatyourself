7b0e3841574f8e73f5b53c9175d49ba00365e159
ZainW <zainthegreatcb@gmail.com>
2015-11-19 14:40:16 -0500

    remove fixtures, add restaurant test

---
+++ test/factories/restaurants.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+	factory :restaurant do 
+		name "Smokes"
+		cuisine "French"
+		description "A palce where you get poutine"
+		location "123 somewhere street"
+	end
+	
+end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -4,4 +4,10 @@ class RestaurantTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+  	@restaurant = FactoryGirl.create(:restaurant)
+  end
+  test "existance" do
+  	assert @restaurant
+  end
 end

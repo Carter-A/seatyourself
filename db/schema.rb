b40ef516f4ab705768ef5ff56c927c25d2f09821
ZainW <zainthegreatcb@gmail.com>
2015-11-19 16:46:21 -0500

    change time to int

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118210621) do
+ActiveRecord::Schema.define(version: 20151119213909) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "party_size"
-    t.datetime "time"
     t.integer  "user_id"
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "time"
   end
 
   create_table "restaurants", force: :cascade do |t|

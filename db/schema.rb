17c970c77890aad0d4544d69b5cd138aae37de2c
ZainW <zainthegreatcb@gmail.com>
2015-11-20 13:53:57 -0500

    add date datatype to reservations schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119213909) do
+ActiveRecord::Schema.define(version: 20151120184856) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "party_size"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151119213909) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "time"
+    t.date     "date"
   end
 
   create_table "restaurants", force: :cascade do |t|
